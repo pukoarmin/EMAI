aad986c580247b0ce93ae67194b010e2f15aeca9
Purle-Kopacz Armin <pukoarmin@gmail.com>
2023-11-01 17:32:57 +0100

    quan

--- save_model.py
+++ save_model.py
@@ -27,3 +27,33 @@ if __name__ == "__main__":
 	
     model = Architecture.SweepNet(int(height), int(width))
     tf.saved_model.save(model, direct)
+
+    q_ds = tf.keras.preprocessing.image_dataset_from_directory(
+                "C:\Users\pukoa\Documents\EMSYS_Master\EMAI\SweepNet\DATASETS\D1\\train\images",
+                label_mode='categorical',
+                validation_split=0.5,
+                subset="training",
+                color_mode="grayscale",
+                image_size=(height, width),
+                seed=123,
+                batch_size=10)
+    ev_ds = tf.keras.preprocessing.image_dataset_from_directory(
+                "C:\Users\pukoa\Documents\EMSYS_Master\EMAI\SweepNet\DATASETS\D1\\test\images",
+                label_mode='categorical',
+                validation_split=0.5,
+                subset="training",
+                color_mode="grayscale",
+                image_size=(height, width),
+                seed=123,
+                batch_size=10)
+
+    #quantize model
+    from tensorflow_model_optimization.quantization.keras import vitis_quantize
+    quantizer = vitis_quantize.VitisQuantizer(model)
+    quantized_model = quantizer.quantize_model(calib_dataset=q_ds, 
+                                               calib_step=100, 
+                                               calib_batch_size=10) 
+    quantized_model.save('quantized_model.h5')
+    quantized_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics= keras.metrics.SparseTopKCategoricalAccuracy())
+    quantized_model.evaluate(ev_ds)
