7782b5e1f7443a49d6c7a7426e544eecf2ff51a3
Purle-Kopacz Armin <pukoarmin@gmail.com>
2023-11-27 17:38:08 +0100

    Quatization warning forfiet, error fix for loading

--- Logic/Architecture.py
+++ Logic/Architecture.py
@@ -15,13 +15,13 @@ def SE_block(x_0, r=16):
     x = tf.keras.layers.Flatten()(x)
 
     # Reshape to add two new dimensions
-    x = tf.reshape(x, (tf.shape(x)[0], 1, 1, tf.shape(x)[-1]))
+    x = layers.reshape(x, (tf.shape(x)[0], 1, 1, tf.shape(x)[-1]))
 
     x = layers.Conv2D(filters=channels // r, kernel_size=1, strides=1)(x)
     x = layers.Activation('relu')(x)
     x = layers.Conv2D(filters=channels, kernel_size=1, strides=1)(x)
     x = layers.Activation('sigmoid')(x)
-    x = x_0 * x
+    x = layers.Multiply()([x_0, x])
 
     return x
 
